822548b1e3075fd9e84971d88ebc6a5acb7850ee
Anton <altay77@inbox.ru>
2022-09-18 20:48:12 +0700

    Task7 solution

---
+++ Task7_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+void PrintArray(double[,] table)
+            {
+            for (int i = 0; i < table.GetLength(0); i++)
+                {
+                    for (int j = 0; j < table.GetLength(1); j++)
+                        {
+                            Console.Write(table[i,j] + "\t" );
+                        }
+                    Console.WriteLine();
+                }
+            }
+double[,] FillArray (int m, int n)
+            {
+                double[,] array = new double[m,n];
+                for (int i=0;i<m;i++)
+                    {
+                        for (int j=0;j<n;j++)
+                            array[i,j] = Math.Round(new Random().Next(-100,100)+ new Random().NextDouble(),2);
+                    }
+                return array;
+            }
+Console.Write ("Введите количество строк двумерного массива ");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите количество стобцов двумерного массива ");
+int n = Convert.ToInt32 (Console.ReadLine());       
+double[,] mas = FillArray(m,n);
+PrintArray(mas);
